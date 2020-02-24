327b56310e6c65262cfa55e95e20ba0092ba9d9e
Hiroaki Nakamura <hnakamur@gmail.com>
2020-02-24 16:54:00 +0900

    Restart from scratch and add functions for String

--- key.go
+++ key.go
@@ -2,139 +2,130 @@
 // Two encoded key bytes of two different keys keeps the order
 // so that you can use encoded key bytes for keys in a key-value-store
 // which is capable to do range scans.
+//
+// The encoding in this package is a subset of the FDB Tuple layer typecodes encoding.
+// https://github.com/apple/foundationdb/blob/92b41e3562e639e16dbe0142cc479a3304e9c08a/design/tuple.md
+// https://activesphere.com/blog/2018/08/17/order-preserving-serialization
 package keybytes
 
 import (
 	"bytes"
-	"encoding/binary"
-	"math"
+	"database/sql"
+	"errors"
+	"io"
+	"strings"
 )
 
-// AppendRaw appends a raw bytes value to dst.
-func AppendRaw(dst, value []byte) []byte {
-	return append(dst, value...)
-}
+const (
+	typeCodeNull          = 0x00
+	typeCodeUTF8String    = 0x02
+	typeCodeNegativeInt64 = 0x0C
+	typeCodeNegativeInt32 = 0x0F
+	typeCodeIntZero       = 0x14
+	typeCodePositiveInt32 = 0x19
+	typeCodePositiveInt64 = 0x1C
+	typeCodeFloat64       = 0x21
+	typeCodeFalse         = 0x26
+	typeCodeTrue          = 0x27
+)
 
-// AppendByte appends a byte value to dst.
-func AppendByte(dst []byte, value byte) []byte {
-	return append(dst, value)
-}
+var errUnpexptedTypeCode = errors.New("unexpected type code")
 
-// AppendByte appends a byte value to dst for descending order range scan.
-func AppendByteDesc(dst []byte, value byte) []byte {
-	return append(dst, value^math.MaxUint8)
-}
-
-// AppendUint16 appends a uint16 value to dst.
-func AppendUint16(dst []byte, value uint16) []byte {
-	var b [2]byte
-	binary.BigEndian.PutUint16(b[:], value)
-	return append(dst, b[:]...)
-}
-
-// AppendUint16Desc appends a uint16 value to dst for descending order range scan.
-func AppendUint16Desc(dst []byte, value uint16) []byte {
-	var b [2]byte
-	binary.BigEndian.PutUint16(b[:], value^math.MaxUint16)
-	return append(dst, b[:]...)
-}
-
-// AppendUint32 appends a uint32 value to dst.
-func AppendUint32(dst []byte, value uint32) []byte {
-	var b [4]byte
-	binary.BigEndian.PutUint32(b[:], value)
-	return append(dst, b[:]...)
-}
-
-// AppendUint32Desc appends a uint32 value to dst for descending order range scan.
-func AppendUint32Desc(dst []byte, value uint32) []byte {
-	var b [4]byte
-	binary.BigEndian.PutUint32(b[:], value^math.MaxUint32)
-	return append(dst, b[:]...)
-}
-
-// AppendUint64 appends a uint64 value to dst.
-func AppendUint64(dst []byte, value uint64) []byte {
-	var b [8]byte
-	binary.BigEndian.PutUint64(b[:], value)
-	return append(dst, b[:]...)
-}
-
-// AppendUint64Desc appends a uint64 value to dst for descending order range scan.
-func AppendUint64Desc(dst []byte, value uint64) []byte {
-	var b [8]byte
-	binary.BigEndian.PutUint64(b[:], value^math.MaxUint64)
-	return append(dst, b[:]...)
-}
-
-// AppendStringNul appends a string value and a nul byte '\x00' to dst.
-// The value must not contain a nul byte in supposed usage of this function,
-// but ensuring that is caller's responsibility.
-func AppendStringNul(dst []byte, value string) []byte {
-	dst = append(dst, []byte(value)...)
-	return append(dst, '\x00')
-}
-
-// TakeRaw takes the first n bytes from the key and returns it and the rest of the key.
-// It panics if the length of the key is smaller than n.
-func TakeRaw(key []byte, n int) (value []byte, rest []byte) {
-	return key[:n], key[n:]
-}
-
-// TakeByte takes the first byte from the key and returns it and the rest of the key.
-// It panics if the length of the key is smaller than 1.
-func TakeByte(key []byte) (value byte, rest []byte) {
-	return key[0], key[1:]
-}
-
-// TakeByteDesc takes the first byte for descending order range scan from the key and returns it and the rest of the key.
-// It panics if the length of the key is smaller than 1.
-func TakeByteDesc(key []byte) (value byte, rest []byte) {
-	return key[0] ^ math.MaxUint8, key[1:]
-}
-
-// TakeUint16 takes a uint16 value from the beginning of the key and returns it and the rest of the key.
-// It panics if the length of the key is smaller than 2.
-func TakeUint16(key []byte) (value uint16, rest []byte) {
-	return binary.BigEndian.Uint16(key[:2]), key[2:]
-}
-
-// TakeUint16Desc takes a uint16 value for descending order range scan from the beginning of the key and returns it and the rest of the key.
-// It panics if the length of the key is smaller than 2.
-func TakeUint16Desc(key []byte) (value uint16, rest []byte) {
-	return binary.BigEndian.Uint16(key[:2]) ^ math.MaxUint16, key[2:]
-}
-
-// TakeUint32 takes a uint32 value from the beginning of the key and returns it and the rest of the key.
-// It panics if the length of the key is smaller than 4.
-func TakeUint32(key []byte) (value uint32, rest []byte) {
-	return binary.BigEndian.Uint32(key[:4]), key[4:]
-}
-
-// TakeUint32Desc takes a uint32 value for descending order range scan from the beginning of the key and returns it and the rest of the key.
-// It panics if the length of the key is smaller than 4.
-func TakeUint32Desc(key []byte) (value uint32, rest []byte) {
-	return binary.BigEndian.Uint32(key[:4])^math.MaxUint32, key[4:]
-}
-
-// TakeUint64 takes a uint64 value from the beginning of the key and returns it and the rest of the key.
-// It panics if the length of the key is smaller than 8.
-func TakeUint64(key []byte) (value uint64, rest []byte) {
-	return binary.BigEndian.Uint64(key[:8]), key[8:]
-}
-
-// TakeUint64Desc takes a uint64 value for descending order range scan from the beginning of the key and returns it and the rest of the key.
-// It panics if the length of the key is smaller than 4.
-func TakeUint64Desc(key []byte) (value uint64, rest []byte) {
-	return binary.BigEndian.Uint64(key[:8])^math.MaxUint64, key[8:]
-}
-
-// TakeStringNul takes a string value before the first nul byte '\x00' and the first nul byte from the beginning of the key and returns the string and the rest of the key.
-// It panics if the key does not contain a nul byte '\x00'
-func TakeStringNul(key []byte) (value string, rest []byte) {
-	i := bytes.IndexByte(key, '\x00')
-	if i == -1 {
-		panic("a nul byte not found in key")
+// AppendNullString appends a NullString value to dst.
+func AppendNullString(dst []byte, value sql.NullString) []byte {
+	if value.Valid {
+		return AppendString(dst, value.String)
 	}
-	return string(key[:i]), key[i+1:]
+	return append(dst, typeCodeNull)
+}
+
+// AppendString appends a string value to dst.
+func AppendString(dst []byte, value string) []byte {
+	dst = append(dst, typeCodeUTF8String)
+	for {
+		i := strings.IndexByte(value, '\x00')
+		if i == -1 {
+			return append(append(dst, value...), '\x00')
+		}
+
+		dst = append(append(dst, value[:i+1]...), '\xFF')
+		value = value[i+1:]
+	}
+}
+
+// TakeString takes a NullString value from b and returns it and the rest of b.
+func TakeNullString(b []byte) (value sql.NullString, rest []byte, err error) {
+	var c byte
+	c, rest, err = takeTypeCode(b)
+	if err != nil {
+		return value, b, err
+	}
+	switch c {
+	case typeCodeNull:
+		return value, b[1:], nil
+	case typeCodeUTF8String:
+		s, rest, err := takeStringValue(rest)
+		if err != nil {
+			return value, b, err
+		}
+		return sql.NullString{Valid: true, String: s}, rest, nil
+	default:
+		return value, b, errUnpexptedTypeCode
+	}
+}
+
+// TakeString takes a string value from b and returns it and the rest of b.
+func TakeString(b []byte) (value string, rest []byte, err error) {
+	rest, err = expectTypeCode(b, typeCodeUTF8String)
+	if err != nil {
+		return "", b, err
+	}
+	value, rest, err = takeStringValue(rest)
+	if err != nil {
+		return "", b, err
+	}
+	return value, rest, nil
+}
+
+func takeStringValue(src []byte) (value string, rest []byte, err error) {
+	var out []byte
+	for {
+		i := bytes.IndexByte(src, '\x00')
+		if i == -1 {
+			return "", nil, io.ErrUnexpectedEOF
+		}
+
+		if i+1 < len(src) && src[i+1] == '\xFF' {
+			if out == nil {
+				out = []byte{}
+			}
+			out = append(out, src[:i+1]...)
+			src = src[i+2:]
+			continue
+		}
+
+		if out == nil {
+			return string(src[:i]), src[i+1:], nil
+		}
+		return string(append(out, src[:i]...)), src[i+1:], nil
+	}
+}
+
+func expectTypeCode(b []byte, typeCode byte) (rest []byte, err error) {
+	var c byte
+	c, rest, err = takeTypeCode(b)
+	if err != nil {
+		return b, err
+	}
+	if c != typeCode {
+		return b, errUnpexptedTypeCode
+	}
+	return rest, nil
+}
+
+func takeTypeCode(b []byte) (typeCode byte, rest []byte, err error) {
+	if len(b) < 1 {
+		return 0, b, io.ErrUnexpectedEOF
+	}
+	return b[0], b[1:], nil
 }
--- key_test.go
+++ key_test.go
@@ -2,318 +2,121 @@ package keybytes_test
 
 import (
 	"bytes"
-	"math"
+	"database/sql"
+	"reflect"
 	"testing"
 
 	"github.com/hnakamur/keybytes"
 )
 
-func TestAppendRaw(t *testing.T) {
-	t.Run("emptyDst", func(t *testing.T) {
-		key := []byte("foo")
-		var dst []byte
-		dst = keybytes.AppendRaw(dst, key)
-
-		key2, rest := keybytes.TakeRaw(dst, len(key))
-		if got, want := key2, key; !bytes.Equal(got, want) {
-			t.Errorf("decoded key unmatch, got=%s, want=%s", string(got), string(want))
+func TestAppendNullString(t *testing.T) {
+	t.Run("roundtrip", func(t *testing.T) {
+		testCases := []sql.NullString{
+			{Valid: false, String: ""},
+			{Valid: true, String: ""},
+			{Valid: true, String: "foo"},
+			{Valid: true, String: "F\u00d4O\u0000bar"},
+			{Valid: true, String: "\u0000foo"},
+			{Valid: true, String: "foo\u0000"},
+			{Valid: true, String: "f\x00\x00oo"},
+			{Valid: true, String: "\x00"},
+			{Valid: true, String: "\x00\x00"},
+			{Valid: true, String: "\xff"},
+			{Valid: true, String: "\xff\xff"},
+			{Valid: true, String: "\x00\xff"},
+			{Valid: true, String: "\x00\x00\xff\xff"},
 		}
-		if got, want := len(rest), 0; got != want {
-			t.Errorf("rest length unmatch, got=%v, want=%v", got, want)
-		}
-	})
-	t.Run("shortCapDst", func(t *testing.T) {
-		key := []byte("foo")
-		dst := make([]byte, 0, 2)
-		dst = keybytes.AppendRaw(dst, key)
-
-		key2, rest := keybytes.TakeRaw(dst, len(key))
-		if got, want := key2, key; !bytes.Equal(got, want) {
-			t.Errorf("decoded key unmatch, got=%s, want=%s", string(got), string(want))
-		}
-		if got, want := len(rest), 0; got != want {
-			t.Errorf("rest length unmatch, got=%v, want=%v", got, want)
-		}
-	})
-	t.Run("longCapDst", func(t *testing.T) {
-		key := []byte("foo")
-		dst := make([]byte, 0, 3)
-		dst = keybytes.AppendRaw(dst, key)
-
-		key2, rest := keybytes.TakeRaw(dst, len(key))
-		if got, want := key2, key; !bytes.Equal(got, want) {
-			t.Errorf("decoded key unmatch, got=%s, want=%s", string(got), string(want))
-		}
-		if got, want := len(rest), 0; got != want {
-			t.Errorf("rest length unmatch, got=%v, want=%v", got, want)
-		}
-	})
-}
-
-func TestAppendByte(t *testing.T) {
-	t.Run("encodeDecode", func(t *testing.T) {
-		for k := byte(0); ; k++ {
-			dst := keybytes.AppendByte([]byte{}, k)
-			k2, rest := keybytes.TakeByte(dst)
-			if got, want := k2, k; got != want {
-				t.Errorf("decoded key unmatch, got=%v, want=%v", got, want)
+		for i, input := range testCases {
+			b := keybytes.AppendNullString([]byte(nil), input)
+			s, rest, err := keybytes.TakeNullString(b)
+			if err != nil {
+				t.Errorf("case %d: got error: %s", i, err)
+			}
+			if got, want := s, input; !reflect.DeepEqual(got, want) {
+				t.Errorf("case %d: string unmatch: got=%+v, want=%+v", i, got, want)
 			}
 			if got, want := len(rest), 0; got != want {
-				t.Errorf("rest length unmatch, got=%v, want=%v", got, want)
-			}
-
-			if k == math.MaxUint8 {
-				break
+				t.Errorf("case %d: rest length unmatch: got=%d, want=%d", i, got, want)
 			}
 		}
 	})
-	t.Run("keepsOrder", func(t *testing.T) {
-		for k1 := byte(0); k1 < math.MaxUint8; k1++ {
-			k2 := k1 + 1
-			kb1 := keybytes.AppendByte([]byte{}, k1)
-			kb2 := keybytes.AppendByte([]byte{}, k2)
-			if got, want := bytes.Compare(kb1, kb2), -1; got != want {
-				t.Errorf("unexpected compare result, got=%d, want=%d, k1=%v, k2=%v, kb1=%v, kb2=%v",
-					got, want, k1, k2, kb1, kb2)
-			}
-		}
-	})
-}
-
-func TestAppendByteDesc(t *testing.T) {
-	t.Run("encodeDecode", func(t *testing.T) {
-		for k := byte(0); ; k++ {
-			dst := keybytes.AppendByteDesc([]byte{}, k)
-			k2, rest := keybytes.TakeByteDesc(dst)
-			if got, want := k2, k; got != want {
-				t.Errorf("decoded key unmatch, got=%v, want=%v", got, want)
-			}
-			if got, want := len(rest), 0; got != want {
-				t.Errorf("rest length unmatch, got=%v, want=%v", got, want)
-			}
-
-			if k == math.MaxUint8 {
-				break
-			}
-		}
-	})
-	t.Run("keepsOrder", func(t *testing.T) {
-		for k1 := byte(0); k1 < math.MaxUint8; k1++ {
-			k2 := k1 + 1
-			kb1 := keybytes.AppendByteDesc([]byte{}, k1)
-			kb2 := keybytes.AppendByteDesc([]byte{}, k2)
-			if got, want := bytes.Compare(kb1, kb2), 1; got != want {
-				t.Errorf("unexpected compare result, got=%d, want=%d, k1=%v, k2=%v, kb1=%v, kb2=%v",
-					got, want, k1, k2, kb1, kb2)
-			}
-		}
-	})
-}
-
-func TestAppendUint16(t *testing.T) {
-	t.Run("encodeDecode", func(t *testing.T) {
-		for k := uint16(0); ; k++ {
-			dst := keybytes.AppendUint16([]byte{}, k)
-			k2, rest := keybytes.TakeUint16(dst)
-			if got, want := k2, k; got != want {
-				t.Errorf("decoded key unmatch, got=%v, want=%v", got, want)
-			}
-			if got, want := len(rest), 0; got != want {
-				t.Errorf("rest length unmatch, got=%v, want=%v", got, want)
-			}
-
-			if k == math.MaxUint16 {
-				break
-			}
-		}
-	})
-	t.Run("keepsOrder", func(t *testing.T) {
-		for k1 := uint16(0); k1 < math.MaxUint16; k1++ {
-			k2 := k1 + 1
-			kb1 := keybytes.AppendUint16([]byte{}, k1)
-			kb2 := keybytes.AppendUint16([]byte{}, k2)
-			if got, want := bytes.Compare(kb1, kb2), -1; got != want {
-				t.Errorf("unexpected compare result, got=%d, want=%d, k1=%v, k2=%v, kb1=%v, kb2=%v",
-					got, want, k1, k2, kb1, kb2)
-			}
-		}
-	})
-}
-
-func TestAppendUint16Desc(t *testing.T) {
-	t.Run("encodeDecode", func(t *testing.T) {
-		for k := uint16(0); ; k++ {
-			dst := keybytes.AppendUint16Desc([]byte{}, k)
-			k2, rest := keybytes.TakeUint16Desc(dst)
-			if got, want := k2, k; got != want {
-				t.Errorf("decoded key unmatch, got=%v, want=%v", got, want)
-			}
-			if got, want := len(rest), 0; got != want {
-				t.Errorf("rest length unmatch, got=%v, want=%v", got, want)
-			}
-
-			if k == math.MaxUint16 {
-				break
-			}
-		}
-	})
-	t.Run("keepsOrder", func(t *testing.T) {
-		for k1 := uint16(0); k1 < math.MaxUint16; k1++ {
-			k2 := k1 + 1
-			kb1 := keybytes.AppendUint16Desc([]byte{}, k1)
-			kb2 := keybytes.AppendUint16Desc([]byte{}, k2)
-			if got, want := bytes.Compare(kb1, kb2), 1; got != want {
-				t.Errorf("unexpected compare result, got=%d, want=%d, k1=%v, k2=%v, kb1=%v, kb2=%v",
-					got, want, k1, k2, kb1, kb2)
-			}
-		}
-	})
-}
-
-func TestAppendUint32(t *testing.T) {
-	t.Run("encodeDecode", func(t *testing.T) {
-		testCases := []uint32{0, 1, math.MaxUint32 - 1, math.MaxUint32}
-		for _, k := range testCases {
-			dst := keybytes.AppendUint32([]byte{}, k)
-			k2, rest := keybytes.TakeUint32(dst)
-			if got, want := k2, k; got != want {
-				t.Errorf("decoded key unmatch, got=%v, want=%v", got, want)
-			}
-			if got, want := len(rest), 0; got != want {
-				t.Errorf("rest length unmatch, got=%v, want=%v, k=%d", got, want, k)
-			}
-		}
-	})
-	t.Run("keepsOrder", func(t *testing.T) {
-		testCases := []uint32{0, 1, math.MaxUint32 - 2, math.MaxUint32 - 1}
-		for _, k1 := range testCases {
-			k2 := k1 + 1
-			kb1 := keybytes.AppendUint32([]byte{}, k1)
-			kb2 := keybytes.AppendUint32([]byte{}, k2)
-			if got, want := bytes.Compare(kb1, kb2), -1; got != want {
-				t.Errorf("unexpected compare result, got=%d, want=%d, k1=%v, k2=%v, kb1=%v, kb2=%v",
-					got, want, k1, k2, kb1, kb2)
-			}
-		}
-	})
-}
-
-func TestAppendUint32Desc(t *testing.T) {
-	t.Run("encodeDecode", func(t *testing.T) {
-		testCases := []uint32{0, 1, math.MaxUint32 - 1, math.MaxUint32}
-		for _, k := range testCases {
-			dst := keybytes.AppendUint32Desc([]byte{}, k)
-			k2, rest := keybytes.TakeUint32Desc(dst)
-			if got, want := k2, k; got != want {
-				t.Errorf("decoded key unmatch, got=%v, want=%v", got, want)
-			}
-			if got, want := len(rest), 0; got != want {
-				t.Errorf("rest length unmatch, got=%v, want=%v, k=%d", got, want, k)
-			}
-		}
-	})
-	t.Run("keepsOrder", func(t *testing.T) {
-		testCases := []uint32{0, 1, math.MaxUint32 - 2, math.MaxUint32 - 1}
-		for _, k1 := range testCases {
-			k2 := k1 + 1
-			kb1 := keybytes.AppendUint32Desc([]byte{}, k1)
-			kb2 := keybytes.AppendUint32Desc([]byte{}, k2)
-			if got, want := bytes.Compare(kb1, kb2), 1; got != want {
-				t.Errorf("unexpected compare result, got=%d, want=%d, k1=%v, k2=%v, kb1=%v, kb2=%v",
-					got, want, k1, k2, kb1, kb2)
-			}
-		}
-	})
-}
-
-func TestAppendUint64(t *testing.T) {
-	t.Run("encodeDecode", func(t *testing.T) {
-		testCases := []uint64{0, 1, math.MaxUint64 - 1, math.MaxUint64}
-		for _, k := range testCases {
-			dst := keybytes.AppendUint64([]byte{}, k)
-			k2, rest := keybytes.TakeUint64(dst)
-			if got, want := k2, k; got != want {
-				t.Errorf("decoded key unmatch, got=%v, want=%v", got, want)
-			}
-			if got, want := len(rest), 0; got != want {
-				t.Errorf("rest length unmatch, got=%v, want=%v, k=%d", got, want, k)
-			}
-		}
-	})
-	t.Run("keepsOrder", func(t *testing.T) {
-		testCases := []uint64{0, 1, math.MaxUint64 - 2, math.MaxUint64 - 1}
-		for _, k1 := range testCases {
-			k2 := k1 + 1
-			kb1 := keybytes.AppendUint64([]byte{}, k1)
-			kb2 := keybytes.AppendUint64([]byte{}, k2)
-			if got, want := bytes.Compare(kb1, kb2), -1; got != want {
-				t.Errorf("unexpected compare result, got=%d, want=%d, k1=%v, k2=%v, kb1=%v, kb2=%v",
-					got, want, k1, k2, kb1, kb2)
-			}
-		}
-	})
-}
-
-func TestAppendUint64Desc(t *testing.T) {
-	t.Run("encodeDecode", func(t *testing.T) {
-		testCases := []uint64{0, 1, math.MaxUint64 - 1, math.MaxUint64}
-		for _, k := range testCases {
-			dst := keybytes.AppendUint64Desc([]byte{}, k)
-			k2, rest := keybytes.TakeUint64Desc(dst)
-			if got, want := k2, k; got != want {
-				t.Errorf("decoded key unmatch, got=%v, want=%v", got, want)
-			}
-			if got, want := len(rest), 0; got != want {
-				t.Errorf("rest length unmatch, got=%v, want=%v, k=%d", got, want, k)
-			}
-		}
-	})
-	t.Run("keepsOrder", func(t *testing.T) {
-		testCases := []uint64{0, 1, math.MaxUint64 - 2, math.MaxUint64 - 1}
-		for _, k1 := range testCases {
-			k2 := k1 + 1
-			kb1 := keybytes.AppendUint64Desc([]byte{}, k1)
-			kb2 := keybytes.AppendUint64Desc([]byte{}, k2)
-			if got, want := bytes.Compare(kb1, kb2), 1; got != want {
-				t.Errorf("unexpected compare result, got=%d, want=%d, k1=%v, k2=%v, kb1=%v, kb2=%v",
-					got, want, k1, k2, kb1, kb2)
-			}
-		}
-	})
-}
-
-func TestAppendStringNul(t *testing.T) {
-	t.Run("encodeDecode", func(t *testing.T) {
-		testCases := []string{"", "f", "foo"}
-		for _, k := range testCases {
-			dst := keybytes.AppendStringNul([]byte{}, k)
-			k2, rest := keybytes.TakeStringNul(dst)
-			if got, want := k2, k; got != want {
-				t.Errorf("decoded key unmatch, got=%v, want=%v", got, want)
-			}
-			if got, want := len(rest), 0; got != want {
-				t.Errorf("rest length unmatch, got=%v, want=%v, k=%s", got, want, k)
-			}
-		}
-	})
-	t.Run("keepsOrder", func(t *testing.T) {
+	t.Run("order", func(t *testing.T) {
 		testCases := []struct {
-			k1, k2 string
+			a, b sql.NullString
 		}{
-			{k1: "", k2: "b"},
-			{k1: "bar", k2: "bars"},
-			{k1: "bar", k2: "baz"},
-			{k1: "bar", k2: "bb"},
-			{k1: "bar", k2: "c"},
+			{
+				a: sql.NullString{Valid: false, String: ""},
+				b: sql.NullString{Valid: true, String: ""},
+			},
+			{
+				a: sql.NullString{Valid: true, String: ""},
+				b: sql.NullString{Valid: true, String: "a"},
+			},
+			{
+				a: sql.NullString{Valid: true, String: "a"},
+				b: sql.NullString{Valid: true, String: "a\x00"},
+			},
+			{
+				a: sql.NullString{Valid: true, String: "bar"},
+				b: sql.NullString{Valid: true, String: "bb"},
+			},
 		}
-		for _, tc := range testCases {
-			k1 := tc.k1
-			k2 := tc.k2
-			kb1 := keybytes.AppendStringNul([]byte{}, k1)
-			kb2 := keybytes.AppendStringNul([]byte{}, k2)
-			if got, want := bytes.Compare(kb1, kb2), -1; got != want {
-				t.Errorf("unexpected compare result, got=%d, want=%d, k1=%v, k2=%v, kb1=%v, kb2=%v",
-					got, want, k1, k2, kb1, kb2)
+		for i, tc := range testCases {
+			a := keybytes.AppendNullString([]byte(nil), tc.a)
+			b := keybytes.AppendNullString([]byte(nil), tc.b)
+			if got, want := bytes.Compare(a, b), -1; got != want {
+				t.Errorf("case %d: compare result unmatch: got=%d, want=%d, a=0x%x, b=0x%x",
+					i, got, want, a, b)
+			}
+		}
+	})
+}
+
+func TestAppendString(t *testing.T) {
+	t.Run("roundtrip", func(t *testing.T) {
+		testCases := []string{
+			"",
+			"foo",
+			"F\u00d4O\u0000bar",
+			"\u0000foo",
+			"foo\u0000",
+			"f\x00\x00oo",
+			"\x00",
+			"\x00\x00",
+			"\xff",
+			"\xff\xff",
+			"\x00\xff",
+			"\x00\x00\xff\xff",
+		}
+		for i, input := range testCases {
+			b := keybytes.AppendString([]byte(nil), input)
+			s, rest, err := keybytes.TakeString(b)
+			if err != nil {
+				t.Errorf("case %d: got error: %s", i, err)
+			}
+			if got, want := s, input; got != want {
+				t.Errorf("case %d: string unmatch: got=%q, want=%q", i, got, want)
+			}
+			if got, want := len(rest), 0; got != want {
+				t.Errorf("case %d: rest length unmatch: got=%d, want=%d", i, got, want)
+			}
+		}
+	})
+	t.Run("order", func(t *testing.T) {
+		testCases := []struct {
+			a, b string
+		}{
+			{a: "", b: "\x00"},
+			{a: "", b: "a"},
+			{a: "a", b: "a\x00"},
+			{a: "bar", b: "bb"},
+		}
+		for i, tc := range testCases {
+			a := keybytes.AppendString([]byte(nil), tc.a)
+			b := keybytes.AppendString([]byte(nil), tc.b)
+			if got, want := bytes.Compare(a, b), -1; got != want {
+				t.Errorf("case %d: compare result unmatch: got=%d, want=%d, a=0x%x, b=0x%x",
+					i, got, want, a, b)
 			}
 		}
 	})
